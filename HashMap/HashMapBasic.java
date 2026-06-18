7ac0ad49c0044fa2b4d0ca08cbf90cce17f39df4
Kodali Aniketh Kumar <anikethkumark@gmail.com>
2026-06-18 22:53:38 +0530

    used HashMap without builtin data structure

---
+++ HashMap/HashMapBasic.java
@@ -0,0 +1,30 @@
+package HashMap;
+import java.util.*;
+public class HashMapBasic {
+    public static void main(String[] args){
+        // first the arraysize to be taken as input
+        // next the array is taken as input
+        // then the next input is the number of queries
+        // next the queries are given as input
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter the size of the array: ");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        System.out.println("Enter the elements of the array: ");
+        int[] Hash = new int[13];
+        int count = 0;
+        for(int i = 0; i < size; i++){
+            arr[i] = sc.nextInt();
+            Hash[arr[i]]++;
+        }
+        System.out.println("Enter the number of queries");
+        int queries = sc.nextInt();
+        int x = 0;
+        for(int y = 1; y <= queries; y++){
+           x = sc.nextInt();
+           System.out.println(Hash[y]);
+        }
+        
+
+    }
+}
