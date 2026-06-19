f8de5c642022c5584268bdeaa9211276ba62d9e4
Kodali Aniketh Kumar <anikethkumark@gmail.com>
2026-06-19 11:02:14 +0530

    added basic usual functions in HashSet

---
+++ HashMap/HashMapBasFun.java
@@ -0,0 +1,16 @@
+package HashMap;
+import java.util.*;
+
+public class HashMapBasFun {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        HashSet<Integer> Set = new HashSet<>();
+        // to add numbers into the set then 
+        Set.add(77); Set.add(65); Set.add(65); Set.add(86);
+        System.out.println(Set.size());
+        System.out.println(Set.contains(21) + " " +Set.contains(65));
+        Set.remove(65);
+        System.out.println(Set.contains(65));
+        System.out.println(Set.size());
+    }
+}
